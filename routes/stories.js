1ceb9d86ef9206e950703a9ef4cf6ff566849d53
Viorel <viorel_popa89@yahoo.com>
2022-09-27 14:17:55 +0300

    add delete method

--- routes/stories.js
+++ routes/stories.js
@@ -17,7 +17,6 @@ router.get("/add", ensureAuth, (req, res) => {
 router.post("/", ensureAuth, async (req, res) => {
   try {
     req.body.user = req.user.id;
-    console.log(req.body);
     await Story.create(req.body);
     res.redirect("/dashboard");
   } catch (err) {
@@ -48,19 +47,24 @@ router.get("/", ensureAuth, async (req, res) => {
 // @route GET /stories/edit/:id
 
 router.get("/edit/:id", ensureAuth, async (req, res) => {
-  const story = await Story.findOne({
-    _id: req.params.id,
-  }).lean();
-  if (!story) {
-    return res.render("error/404");
-  }
+  try {
+    const story = await Story.findOne({
+      _id: req.params.id,
+    }).lean();
+    if (!story) {
+      return res.render("error/404");
+    }
 
-  if (story.user != req.user.id) {
-    res.redirect("/stories");
-  } else {
-    res.render("stories/edit", {
-      story,
-    });
+    if (story.user != req.user.id) {
+      res.redirect("/stories");
+    } else {
+      res.render("stories/edit", {
+        story,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
   }
 });
 
@@ -68,21 +72,39 @@ router.get("/edit/:id", ensureAuth, async (req, res) => {
 // @route PUT /stories/:id
 
 router.put("/:id", ensureAuth, async (req, res) => {
-  let story = await Story.findOne({ _id: req.params.id }).lean();
+  try {
+    let story = await Story.findOne({ _id: req.params.id }).lean();
 
-  if (!story) {
-    return res.render("error/404");
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect("/stories");
+    } else {
+      story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+
+      res.redirect("/dashboard");
+    }
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
   }
+});
 
-  if (story.user != req.user.id) {
-    res.redirect("/stories");
-  } else {
-    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-      new: true,
-      runValidators: true,
-    });
+// @desc Delete story
+// @route DELETE /stories/:id
 
+router.delete("/:id", ensureAuth, async (req, res) => {
+  try {
+    await Story.remove({ _id: req.params.id });
     res.redirect("/dashboard");
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
   }
 });
 
